b9222f3430e7db22328adf785216101db6cfc579
Pablo Torrecillas <pablotorrecillas234@gmail.com>
2026-05-13 00:51:24 -0600

    feat(alumnos): agregar tipos e interfaces del módulo

---
+++ src/types/alumnos.types.ts
@@ -0,0 +1,31 @@
+export interface Alumno {
+  id_alumno: number
+  matricula: string
+  nombre: string
+  apellido: string
+  email: string
+  id_grupo?: number
+  nombre_grupo?: string
+}
+
+export interface AlumnoInput {
+  matricula: string
+  nombre: string
+  apellido: string
+  email: string
+  id_grupo?: number
+}
+
+export interface PagedAlumnos {
+  page: number
+  size: number
+  totalElements: number
+  totalPages: number
+  content: Alumno[]
+}
+
+export interface AlumnosFiltros {
+  search: string
+  page: number
+  size: number
+}
